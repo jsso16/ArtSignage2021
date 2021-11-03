80db19c38dac93a87e9506f5256e6b551da3c0c5
jsso16 <wjsthwls24@daum.net>
2021-11-03 18:26:41 +0900

    Add Text Animations

---
+++ js/scroll.js
@@ -0,0 +1,38 @@
+// Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, Animation it in
+  $(window).scroll(function() {
+    $('.animated-5').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('typing-demo');
+      }
+    });
+  });
+
+  $(window).scroll(function() {
+    $('.animated-6').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('move');
+      }
+    });
+  });
+
+  $(window).scroll(function() {
+    $('.animated-2').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('reveal');
+      }
+    });
+  });
+});
